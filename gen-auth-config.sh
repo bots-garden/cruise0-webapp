2348a9a598d8a0e576e649dcf551d969737a420f
Philippe Charrière <ph.charriere@gmail.com>
2022-07-12 05:21:30 +0000

    📝 save

--- gen-auth-config.sh
+++ gen-auth-config.sh
@@ -4,9 +4,9 @@ echo "  \"domain\":\"$AUTH0_CONFIG_DOMAIN\"," >> cruise0-spa/auth_config.json
 echo "  \"clientId\":\"$AUTH0_CLIENTID_CRUISE0\"" >> cruise0-spa/auth_config.json
 echo "}" >> cruise0-spa/auth_config.json
 
-echo "{" > cruise0-react-spa/auth_config.json
-echo "  \"domain\":\"$AUTH0_CONFIG_DOMAIN\"," >> cruise0-react-spa/auth_config.json
-echo "  \"clientId\":\"$AUTH0_CLIENTID_CRUISE0\"" >> cruise0-react-spa/auth_config.json
-echo "}" >> cruise0-react-spa/auth_config.json
+#echo "{" > cruise0-react-spa/auth_config.json
+#echo "  \"domain\":\"$AUTH0_CONFIG_DOMAIN\"," >> cruise0-react-spa/auth_config.json
+#echo "  \"clientId\":\"$AUTH0_CLIENTID_CRUISE0\"" >> cruise0-react-spa/auth_config.json
+#echo "}" >> cruise0-react-spa/auth_config.json
 
 
